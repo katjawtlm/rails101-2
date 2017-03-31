3eb3eff6f81f797459a893f9fe8b602ad89928d8
katja <helen_k1@hotmail.com>
2017-03-31 12:34:16 +0800

    join &quit group action

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170331021614) do
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "group_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
